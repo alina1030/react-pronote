8da2566e799e139cc9ac6331648b55dbe933699f
alina1030 <85539852+alina1030@users.noreply.github.com>
2021-12-29 21:34:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import HigherOrderFunctions from './components/hof/HigherOrderFunctions';
+import './App.css';
+import Editor from './components/Editor';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Editor></Editor>
+    </div>
+  );
+}
+
+export default App;
